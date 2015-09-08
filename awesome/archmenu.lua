aff65f97d6f8f314d1a1d0e83427f282058ae53b
Michael Johnston <michael.johnston29@gmail.com>
2015-09-08 16:29:33 +1000

    added awesome-wm config

---
+++ awesome/archmenu.lua
@@ -0,0 +1,188 @@
+ local menue0e4fc6213e8b3593495a7260c3a4c2e = {
+     {"Accerciser", "/usr/bin/accerciser", "/usr/share/icons/hicolor/16x16/apps/accerciser.png" },
+     {"Screen Reader", "orca --replace", "/usr/share/icons/hicolor/16x16/apps/orca.png" },
+ }
+
+ local menu98edb85b00d9527ad5acebe451b3fae6 = {
+     {"7-Zip FM", "7zFM", "/usr/share/icons/hicolor/32x32/apps/p7zip.png" },
+     {"Application Finder", "xfce4-appfinder", "/usr/share/icons/gnome/16x16/actions/gtk-find.png" },
+     {"Archive Manager", "file-roller ", "/usr/share/icons/hicolor/16x16/apps/file-roller.png" },
+     {"Calculator", "gnome-calculator", "/usr/share/icons/gnome/16x16/apps/accessories-calculator.png" },
+     {"Character Map", "gucharmap", "/usr/share/icons/gnome/16x16/apps/accessories-character-map.png" },
+     {"Clocks", "gnome-clocks", "/usr/share/icons/hicolor/16x16/apps/gnome-clocks.png" },
+     {"Desktop Search", "tracker-needle", "/usr/share/icons/gnome/16x16/actions/system-search.png" },
+     {"Disks", "gnome-disks", "/usr/share/icons/hicolor/16x16/apps/gnome-disks.png" },
+     {"Files", "nautilus --new-window ", "/usr/share/icons/gnome/16x16/apps/system-file-manager.png" },
+     {"Font Viewer", "gnome-font-viewer ", "/usr/share/icons/gnome/16x16/apps/preferences-desktop-font.png" },
+     {"IPython Qt console", "ipython qtconsole", "/usr/share/icons/gnome/16x16/status/gnome-netstatus-idle.png" },
+     {"KeePassX", "keepassx ", "/usr/share/pixmaps/keepassx.xpm" },
+     {"LastPass", "lastpass", "/usr/share/icons/lastpass.png" },
+     {"Leafpad", "leafpad ", "/usr/share/icons/hicolor/16x16/apps/leafpad.png" },
+     {"Maps", "gnome-maps", "/usr/share/icons/hicolor/16x16/apps/gnome-maps.png" },
+     {"Notes", "bijiben ", "/usr/share/icons/hicolor/16x16/apps/bijiben.png" },
+     {"Passwords and Keys", "/usr/bin/seahorse", "/usr/share/icons/hicolor/16x16/apps/seahorse.png" },
+     {"Screen Reader", "orca --replace", "/usr/share/icons/hicolor/16x16/apps/orca.png" },
+     {"Screenshot", "gnome-screenshot --interactive", "/usr/share/icons/gnome/16x16/apps/applets-screenshooter.png" },
+     {"Vi IMproved", "gvim -f ", "/usr/share/pixmaps/gvim.png" },
+     {"Weather", "gapplication launch org.gnome.Weather.Application", "/usr/share/icons/hicolor/16x16/apps/org.gnome.Weather.Application.png" },
+     {"Xournal", "xournal "},
+     {"gedit", "gedit ", "/usr/share/icons/gnome/16x16/apps/accessories-text-editor.png" },
+     {"ipython", "xterm -e ipython", "/usr/share/icons/gnome/16x16/status/gnome-netstatus-idle.png" },
+ }
+
+ local menu78059f1898ed518c6ccd6a6392fa82c1 = {
+     {"AUR", "xdg-open https://aur.archlinux.org", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+     {"Bugs", "xdg-open https://bugs.archlinux.org", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+     {"Developers", "xdg-open http://www.archlinux.org/developers/", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+     {"Documentation", "xdg-open https://wiki.archlinux.org/index.php/Official_Arch_Linux_Install_Guide", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+     {"Donate", "xdg-open http://www.archlinux.org/donate/", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+     {"Forum", "xdg-open https://bbs.archlinux.org", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+     {"Homepage", "xdg-open http://www.archlinux.org", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+     {"SVN", "xdg-open http://projects.archlinux.org/svntogit/", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+     {"Schwag", "xdg-open http://www.zazzle.com/archlinux/", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+     {"Wiki", "xdg-open https://wiki.archlinux.org", "/usr/share/icons/hicolor/32x32/apps/arch-logo.png" },
+ }
+
+ local menude7a22a0c94aa64ba2449e520aa20c99 = {
+     {"Cantor", "cantor  -caption Cantor", "/usr/share/icons/hicolor/16x16/apps/cantor.png" },
+     {"GNU Octave", "xterm -e /usr/bin/octave", "///usr/share/octave/3.8.2/imagelib/octave-logo.svg" },
+     {"LibreOffice Math", "libreoffice --math ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-math.png" },
+     {"Sage", "xterm -e /opt/sage/sage -notebook", "/usr/share/pixmaps/sage-notebook.png" },
+ }
+
+ local menu251bd8143891238ecedc306508e29017 = {
+     {"AisleRiot Solitaire", "sol", "/usr/share/icons/hicolor/16x16/apps/gnome-aisleriot.png" },
+     {"Chess", "gnome-chess", "/usr/share/icons/hicolor/16x16/apps/gnome-chess.png" },
+     {"Five or More", "five-or-more", "/usr/share/icons/hicolor/16x16/apps/five-or-more.png" },
+     {"Four-in-a-row", "four-in-a-row", "/usr/share/icons/hicolor/16x16/apps/four-in-a-row.png" },
+     {"Iagno", "iagno", "/usr/share/icons/hicolor/16x16/apps/iagno.png" },
+     {"Klotski", "gnome-klotski", "/usr/share/icons/hicolor/16x16/apps/gnome-klotski.png" },
+     {"Lights Off", "lightsoff"},
+     {"Mahjongg", "gnome-mahjongg", "/usr/share/icons/hicolor/16x16/apps/gnome-mahjongg.png" },
+     {"Mines", "gnome-mines", "/usr/share/icons/hicolor/16x16/apps/gnome-mines.png" },
+     {"Nibbles", "gnome-nibbles", "/usr/share/icons/hicolor/16x16/apps/gnome-nibbles.png" },
+     {"Quadrapassel", "quadrapassel", "/usr/share/icons/hicolor/16x16/apps/quadrapassel.png" },
+     {"Robots", "gnome-robots", "/usr/share/icons/hicolor/16x16/apps/gnome-robots.png" },
+     {"Sudoku", "gnome-sudoku", "/usr/share/icons/hicolor/16x16/apps/gnome-sudoku.png" },
+     {"Swell Foop", "swell-foop", "/usr/share/icons/hicolor/16x16/apps/swell-foop.png" },
+     {"Tali", "tali", "/usr/share/icons/hicolor/16x16/apps/tali.png" },
+     {"Tetravex", "gnome-tetravex", "/usr/share/icons/hicolor/16x16/apps/gnome-tetravex.png" },
+ }
+
+ local menud334dfcea59127bedfcdbe0a3ee7f494 = {
+     {"Dia Diagram Editor", "dia ", "/usr/share/icons/hicolor/16x16/apps/dia.png" },
+     {"Document Viewer", "evince ", "/usr/share/icons/hicolor/16x16/apps/evince.png" },
+     {"Feh", "feh ", "///usr/share/feh/images/feh.png" },
+     {"FontForge", "fontforge ", "/usr/share/icons/hicolor/16x16/apps/fontforge.png" },
+     {"GNU Image Manipulation Program", "gimp-2.8 ", "/usr/share/icons/hicolor/16x16/apps/gimp.png" },
+     {"Image Viewer", "eog ", "/usr/share/icons/hicolor/16x16/apps/eog.png" },
+     {"Inkscape", "inkscape ", "/usr/share/icons/hicolor/16x16/apps/inkscape.png" },
+     {"LibreOffice Draw", "libreoffice --draw ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-draw.png" },
+ }
+
+ local menuc8205c7636e728d448c2774e6a4a944b = {
+     {"Avahi SSH Server Browser", "/usr/bin/bssh", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
+     {"Avahi VNC Server Browser", "/usr/bin/bvnc", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
+     {"Chromium", "chromium ", "/usr/share/icons/hicolor/16x16/apps/chromium.png" },
+     {"Deluge", "deluge-gtk ", "/usr/share/icons/hicolor/16x16/apps/deluge.png" },
+     {"Empathy", "empathy", "/usr/share/icons/hicolor/16x16/apps/empathy.png" },
+     {"FileZilla", "filezilla", "/usr/share/icons/hicolor/16x16/apps/filezilla.png" },
+     {"Firefox", "/usr/lib/firefox/firefox ", "/usr/share/icons/hicolor/16x16/apps/firefox.png" },
+     {"MEGASync", "megasync", "/usr/share/icons/hicolor/32x32/apps/mega.png" },
+     {"Opera", "/usr/bin/opera ", "/usr/share/icons/hicolor/16x16/apps/opera-browser.png" },
+     {"Polari", "polari", "/usr/share/icons/hicolor/16x16/apps/polari.png" },
+     {"Remote Desktop Viewer", "vinagre ", "/usr/share/icons/gnome/16x16/apps/preferences-desktop-remote-desktop.png" },
+     {"Tor-Browser (en-US)", "/usr/bin/tor-browser-en", "///usr/share/pixmaps/tor-browser-en.png" },
+     {"Web", "epiphany ", "/usr/share/icons/gnome/16x16/apps/web-browser.png" },
+ }
+
+ local menudf814135652a5a308fea15bff37ea284 = {
+     {"Dictionary", "gnome-dictionary", "/usr/share/icons/gnome/16x16/apps/accessories-dictionary.png" },
+     {"Document Viewer", "evince ", "/usr/share/icons/hicolor/16x16/apps/evince.png" },
+     {"Evolution", "evolution ", "/usr/share/icons/hicolor/16x16/apps/evolution.png" },
+     {"LibreOffice", "libreoffice ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-startcenter.png" },
+     {"LibreOffice Base", "libreoffice --base ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-base.png" },
+     {"LibreOffice Calc", "libreoffice --calc ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-calc.png" },
+     {"LibreOffice Draw", "libreoffice --draw ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-draw.png" },
+     {"LibreOffice Impress", "libreoffice --impress ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-impress.png" },
+     {"LibreOffice Math", "libreoffice --math ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-math.png" },
+     {"LibreOffice Writer", "libreoffice --writer ", "/usr/share/icons/hicolor/16x16/apps/libreoffice-writer.png" },
+ }
+
+ local menue6f43c40ab1c07cd29e4e83e4ef6bf85 = {
+     {"Accerciser", "/usr/bin/accerciser", "/usr/share/icons/hicolor/16x16/apps/accerciser.png" },
+     {"Anjuta", "anjuta ", "/usr/share/icons/hicolor/16x16/apps/anjuta.png" },
+     {"Database browser", "gda-browser-5.0", "/usr/share/pixmaps/gda-browser-5.0.png" },
+     {"Devhelp", "devhelp", "/usr/share/icons/hicolor/16x16/apps/devhelp.png" },
+     {"Eclipse", "eclipse", "/usr/share/icons/hicolor/16x16/apps/eclipse.png" },
+     {"Emacs", "emacs ", "/usr/share/icons/hicolor/16x16/apps/emacs.png" },
+     {"FLUID", "fluid ", "/usr/share/icons/hicolor/16x16/apps/fluid.png" },
+     {"Glade", "glade ", "/usr/share/icons/hicolor/16x16/apps/glade.png" },
+     {"IPython Qt console", "ipython qtconsole", "/usr/share/icons/gnome/16x16/status/gnome-netstatus-idle.png" },
+     {"OpenJDK Monitoring & Management Console", "/usr/bin/jconsole", "/usr/share/icons/hicolor/16x16/apps/java.png" },
+     {"OpenJDK Policy Tool", "/usr/bin/policytool", "/usr/share/icons/hicolor/16x16/apps/java.png" },
+     {"Qt4 Assistant ", "assistant-qt4", "/usr/share/icons/hicolor/32x32/apps/assistant-qt4.png" },
+     {"Qt4 Designer", "designer-qt4", "/usr/share/icons/hicolor/128x128/apps/designer-qt4.png" },
+     {"Qt4 Linguist ", "linguist-qt4", "/usr/share/icons/hicolor/16x16/apps/linguist-qt4.png" },
+     {"Qt4 QDbusViewer ", "qdbusviewer-qt4", "/usr/share/icons/hicolor/32x32/apps/qdbusviewer-qt4.png" },
+     {"gitg", "gitg --no-wd ", "/usr/share/icons/hicolor/16x16/apps/gitg.png" },
+     {"ipython", "xterm -e ipython", "/usr/share/icons/gnome/16x16/status/gnome-netstatus-idle.png" },
+ }
+
+ local menu80d754c83abbbd4ef5d150904efcc86c = {
+     {"R", "xterm -e R", "///usr/share/pixmaps/r.png" },
+ }
+
+ local menu52dd1c847264a75f400961bfb4d1c849 = {
+     {"Audacity", "audacity ", "/usr/share/icons/hicolor/16x16/apps/audacity.png" },
+     {"Audio Mixer", "xfce4-mixer", "/usr/share/icons/hicolor/16x16/apps/multimedia-volume-control.png" },
+     {"Brasero", "brasero ", "/usr/share/icons/hicolor/16x16/apps/brasero.png" },
+     {"Cheese", "cheese", "/usr/share/icons/hicolor/16x16/apps/cheese.png" },
+     {"GNOME ALSA Mixer", "gnome-alsamixer", "///usr/share/pixmaps/gnome-alsamixer/gnome-alsamixer-icon.png" },
+     {"Music", "gnome-music", "/usr/share/icons/hicolor/16x16/apps/gnome-music.png" },
+     {"PNMixer", "pnmixer"},
+     {"PulseAudio Volume Control", "pavucontrol", "/usr/share/icons/hicolor/16x16/apps/multimedia-volume-control.png" },
+     {"Qt V4L2 test Utility", "qv4l2", "/usr/share/icons/hicolor/16x16/apps/qv4l2.png" },
+     {"Rhythmbox", "rhythmbox ", "/usr/share/icons/hicolor/16x16/apps/rhythmbox.png" },
+     {"VLC media player", "/usr/bin/vlc --started-from-file ", "/usr/share/icons/hicolor/16x16/apps/vlc.png" },
+     {"Videos", "totem ", "/usr/share/icons/hicolor/16x16/apps/totem.png" },
+ }
+
+ local menuee69799670a33f75d45c57d1d1cd0ab3 = {
+     {"Avahi Zeroconf Browser", "/usr/bin/avahi-discover", "/usr/share/icons/gnome/16x16/devices/network-wired.png" },
+     {"Bulk Rename", "/usr/lib/Thunar/ThunarBulkRename ", "/usr/share/icons/hicolor/16x16/apps/Thunar.png" },
+     {"Disk Usage Analyzer", "baobab", "/usr/share/icons/hicolor/16x16/apps/baobab.png" },
+     {"File Manager PCManFM", "pcmanfm ", "/usr/share/icons/gnome/16x16/apps/system-file-manager.png" },
+     {"Htop", "xterm -e htop", "/usr/share/pixmaps/htop.png" },
+     {"Logs", "gnome-logs", "/usr/share/icons/hicolor/16x16/apps/gnome-logs.png" },
+     {"MEGASync", "megasync", "/usr/share/icons/hicolor/32x32/apps/mega.png" },
+     {"Network Tools", "gnome-nettool", "/usr/share/icons/hicolor/16x16/apps/gnome-nettool.png" },
+     {"Oracle VM VirtualBox", "VirtualBox ", "/usr/share/icons/hicolor/16x16/mimetypes/virtualbox.png" },
+     {"Sakura", "sakura", "/usr/share/pixmaps/terminal-tango.svg" },
+     {"System Log", "gnome-system-log", "/usr/share/icons/hicolor/16x16/apps/logview.png" },
+     {"System Monitor", "gnome-system-monitor", "/usr/share/icons/gnome/16x16/apps/utilities-system-monitor.png" },
+     {"Terminal", "gnome-terminal", "/usr/share/icons/gnome/16x16/apps/utilities-terminal.png" },
+     {"Thunar File Manager", "thunar ", "/usr/share/icons/hicolor/16x16/apps/Thunar.png" },
+     {"TrueCrypt", "truecrypt", "/usr/share/pixmaps/truecrypt.xpm" },
+     {"UXTerm", "uxterm", "/usr/share/pixmaps/xterm-color_48x48.xpm" },
+     {"Wireshark", "wireshark ", "/usr/share/icons/hicolor/16x16/apps/wireshark.png" },
+     {"XTerm", "xterm", "/usr/share/pixmaps/xterm-color_48x48.xpm" },
+     {"Xfce Terminal", "xfce4-terminal", "/usr/share/icons/gnome/16x16/apps/utilities-terminal.png" },
+     {"dconf Editor", "dconf-editor", "/usr/share/icons/hicolor/16x16/apps/dconf-editor.png" },
+ }
+
+xdgmenu = {
+    {"Accessibility", menue0e4fc6213e8b3593495a7260c3a4c2e},
+    {"Accessories", menu98edb85b00d9527ad5acebe451b3fae6},
+    {"Archlinux", menu78059f1898ed518c6ccd6a6392fa82c1},
+    {"Education", menude7a22a0c94aa64ba2449e520aa20c99},
+    {"Games", menu251bd8143891238ecedc306508e29017},
+    {"Graphics", menud334dfcea59127bedfcdbe0a3ee7f494},
+    {"Internet", menuc8205c7636e728d448c2774e6a4a944b},
+    {"Office", menudf814135652a5a308fea15bff37ea284},
+    {"Programming", menue6f43c40ab1c07cd29e4e83e4ef6bf85},
+    {"Science & Math", menu80d754c83abbbd4ef5d150904efcc86c},
+    {"Sound & Video", menu52dd1c847264a75f400961bfb4d1c849},
+    {"System Tools", menuee69799670a33f75d45c57d1d1cd0ab3},
+}
+
---
+++ awesome/scripts/wallpaper.sh
@@ -0,0 +1,6 @@
+#! /bin/bash 
+
+while true do
+	feh --bg-scale /home/michael/wallpapers/ -z
+	sleep 60
+	
